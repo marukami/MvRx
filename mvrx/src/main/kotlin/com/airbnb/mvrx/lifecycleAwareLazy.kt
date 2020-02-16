593c0ef274c66dcd7c61d5d7312b9e5d090a7091
Mitchell Tilbrook <marukami.kaito@gmail.com>
2020-02-16 13:54:46 +0900

    Update Kotlin and Lifecycle to Java8

--- mvrx/src/main/kotlin/com/airbnb/mvrx/lifecycleAwareLazy.kt
+++ mvrx/src/main/kotlin/com/airbnb/mvrx/lifecycleAwareLazy.kt
@@ -2,11 +2,9 @@
 
 package com.airbnb.mvrx
 
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.OnLifecycleEvent
 import androidx.annotation.RestrictTo
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import java.io.Serializable
 
 private object UninitializedValue
@@ -16,7 +14,7 @@ private object UninitializedValue
  */
 @RestrictTo(RestrictTo.Scope.LIBRARY)
 @SuppressWarnings("Detekt.ClassNaming")
-class lifecycleAwareLazy<out T>(private val owner: LifecycleOwner, initializer: () -> T) : Lazy<T>, Serializable {
+class lifecycleAwareLazy<out T>(owner: LifecycleOwner, initializer: () -> T) : Lazy<T>, Serializable, DefaultLifecycleObserver {
     private var initializer: (() -> T)? = initializer
     @Volatile
     @SuppressWarnings("Detekt.VariableNaming")
@@ -25,9 +23,9 @@ class lifecycleAwareLazy<out T>(private val owner: LifecycleOwner, initializer: 
     private val lock = this
 
     init {
-        owner.lifecycle.addObserver(object : LifecycleObserver {
-            @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
-            fun onStart() {
+        owner.lifecycle.addObserver(object : DefaultLifecycleObserver {
+            override fun onCreate(owner: LifecycleOwner) {
+                super.onCreate(owner)
                 if (!isInitialized()) value
                 owner.lifecycle.removeObserver(this)
             }
